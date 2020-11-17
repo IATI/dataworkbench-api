6bc911e6b489e8c415bf87f308c55b0d68cbb159
Alex Lydiate <alexl@devinit.org>
2020-11-17 15:17:45 +0000

    All stats routes working

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -5,8 +5,91 @@ mongoose.connect(googleStorageConfig.datastore.mongourl, {useNewUrlParser: true,
 const monDataset = require('../../common/mongoose/dataset');
 const monTestDataset = require('../../common/mongoose/testdataset');
 const monPublisherStats = require('../../common/mongoose/publisherstats');
+const monAggregateStats = require('../../common/mongoose/aggregatestats');
 
 module.exports = function(app) {
+
+  //Get percentage of publishers which increased | decreased | unchanged their stats betweem two dates
+  app.get('/api/v1/stats/progress', async function(req, res) {
+
+    if ( ! req.query.start) {
+      return res.status(400).send('missing required parameter: start');
+    }
+
+    if ( ! req.query.end) {
+      return res.status(400).send('missing required parameter: end');
+    }
+
+    let where = { date: req.query.start };
+    let startStats = await monPublisherStats.find(where);
+
+    where = { date: req.query.end };
+    let endStats = await monPublisherStats.find(where);
+
+    let total = {};
+    let increased = {};
+    let decreased = {};
+
+    for (let i=0; i < startStats.length; i++) {
+      let startStat = startStats[i];
+      let endStat = null
+        for (let n=0; i < endStats.length && endStat === null; n++) {
+          if (endStats[n].publisher === startStat.publisher) {
+            endStat = endStats[n];
+            endStats.splice[n, 1];
+          }
+        }
+
+        if (endStat === null) {
+          continue;
+        }
+  
+        for(key in startStat.summaryStats) {
+          if (! (key in total)) {
+            total[key] = 0;
+            increased[key] = 0;
+            decreased[key] = 0;
+          }
+
+          total[key]++;
+  
+          if (startStat.summaryStats[key] > endStat.summaryStats[key]) {
+            decreased[key]++;
+          }
+  
+          if (startStat.summaryStats[key] < endStat.summaryStats[key]) {
+            increased[key]++;
+          }
+        }
+    }
+
+    let stats = {};
+
+    for (key in total) {    
+      stats[key] = {}; 
+      stats[key]['percPublishersIncreased'] = increased[key] * (100 / total[key]);
+      stats[key]['percPublishersDecreased'] = increased[key] * (100 / total[key]);
+      stats[key]['percPublishersUnchanged'] = 100 - stats[key]['percPublishersIncreased'] - stats[key]['percPublishersDecreased'];
+    }
+
+    return res.send(stats);
+  });
+
+  //Get aggregate stats for a specific date
+  app.get('/api/v1/stats/summary', async function(req, res) {
+
+    if ( ! req.query.date) {
+      return res.status(400).send('missing required parameter: date');
+    }
+
+    let where = { date: req.query.date };
+    let stats = await monAggregateStats.findOne(where);
+
+    return res.send(stats);
+  });
+
+
+  //all publisher stats for a given date
   app.get('/api/v1/stats/:publisher', async function(req, res) {
 
     let where = { "publisher": req.params.publisher };
@@ -23,6 +106,41 @@ module.exports = function(app) {
 
     return res.send(stats);
   });
+
+  //all stats for a specific publisher, optional date range
+  app.get('/api/v1/stats/', async function(req, res) {
+
+    if ( ! req.query.date) {
+      return res.status(400).send('missing required parameter: date');
+    } 
+
+    let where = {};
+
+    if (req.query.date) {
+      where['date'] = req.query.date;
+    }
+
+    let stats = await monPublisherStats.find(where);
+
+    return res.send(stats);
+  });
+
+  app.get('/api/v1/stats/delta', async function(req, res) {
+
+    if ( !req.query.date) {
+      return res.status(400).send('missing required parameter: date');
+    }
+
+    let where = {};
+
+    if (req.query.date) {
+      where['date'] = req.query.date;
+    }
+
+    let stats = await monPublisherStats.find(where);
+
+    return res.send(stats);
+  }); 
 
   app.get('/api/v1/queue/next', async function(req, res) {
     let ds = await monDataset.findOne({"json-updated": { "$exists" : false }}).sort({ 'downloaded' : "asc"});
