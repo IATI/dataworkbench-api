c0ef64f114fdd077550a35a457d28b608f2068ae
Alex Lydiate <alexl@devinit.org>
2021-06-25 16:51:29 +0100

    Integrated with unified platform instead of datastore

--- server/boot/jobs.js
+++ server/boot/jobs.js
@@ -5,6 +5,6 @@ if (process.env.RUN_JOBS === 'run') {
 
   // eslint-disable-next-line
   require('./jobs/datastore');
-  require('./jobs/registry');
-  require('./jobs/statsgenerator');
+  //require('./jobs/registry');
+  //require('./jobs/statsgenerator');
 }
